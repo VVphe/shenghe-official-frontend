ce5e47669240cce0666eb3d45579fd3f6ee05de9
Vv <crazyphe@outlook.com>
2019-12-13 19:10:50 +0800

    参数筛选组件

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -7,9 +7,11 @@ import { CategoryTreeComponent } from "./category-tree/category-tree.component";
 import { SharedModule } from "../shared/shared.module";
 import { SliderComponent } from "./slider/slider.component";
 import { FormsModule } from "@angular/forms";
+import { ParamFilterComponent } from './param-filter/param-filter.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
 
 @NgModule({
-  declarations: [ProductComponent, CategoryTreeComponent, SliderComponent],
+  declarations: [ProductComponent, CategoryTreeComponent, SliderComponent, ParamFilterComponent, ProductInfoComponent],
   imports: [SharedModule, ProductRoutingModule, FormsModule]
 })
 export class ProductModule {}
--- src/app/product/slider/slider.component.ts
+++ src/app/product/slider/slider.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged } from "rxjs/operators";
 
 @Component({
   selector: "app-slider",
@@ -14,17 +16,32 @@ export class SliderComponent implements OnInit {
   @Input()
   divisions: number = 20;
 
+  @Input()
+  title = "test";
+
+  @Output()
+  valueChange = new EventEmitter<number[]>();
+
   divisionArr: number[];
 
   value;
+
+  private value$ = new Subject<number[]>();
 
   constructor() {}
 
   ngOnInit() {
     this.divisionArr = Array(this.divisions + 1).fill(0);
+    this.value$
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(value => this.valueChange.emit(value));
   }
 
   median(min: number, max: number): number {
     return Math.round((min + max) / 2);
+  }
+
+  handleValueChange(value: number[]) {
+    this.value$.next(value);
   }
 }
