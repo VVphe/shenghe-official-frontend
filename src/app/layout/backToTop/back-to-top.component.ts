86fe0f1229bd005d8fbe1826b3ad43899fcacdb3
Vv <crazyphe@outlook.com>
2019-12-23 01:34:54 +0800

    联系我们modal & 返回顶部

--- src/app/layout/backToTop/back-to-top.component.ts
+++ src/app/layout/backToTop/back-to-top.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
+import { TimeInterval } from "rxjs/internal/operators/timeInterval";
 
 @Component({
   selector: "app-back-to-top",
@@ -7,7 +8,30 @@ import { Component, OnInit } from "@angular/core";
 })
 export class BackToTopComponent implements OnInit {
   isVisible = false;
+
+  get showBackTopBtn() {
+    return document.documentElement.scrollTop > 200;
+  }
+
+  @HostListener("window:scroll", [])
+  onScrollChange() {}
+
   constructor() {}
 
   ngOnInit() {}
+
+  backToTop() {
+    let iSpeed = 0;
+    let iTimer;
+    clearInterval(iTimer);
+    iTimer = setInterval(() => {
+      const heightTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      iSpeed = Math.floor((0 - heightTop) / 8);
+      document.documentElement.scrollTop = heightTop + iSpeed;
+      if (document.documentElement.scrollTop <= 0) {
+        clearInterval(iTimer);
+      }
+    }, 30);
+  }
 }
