9985eaae764854d6a90607adf87823bcccd46950
Vv <crazyphe@outlook.com>
2020-01-14 09:23:50 +0800

    slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Router } from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   isVisible = false;
+  productIndex = 0;
 
   constructor(private router: Router) {}
 
@@ -19,5 +20,13 @@ export class HomeComponent implements OnInit {
         category
       }
     });
+  }
+
+  pre() {
+    if (this.productIndex > 0) this.productIndex--;
+  }
+
+  next() {
+    if (this.productIndex < 2) this.productIndex++;
   }
 }
